e2f7d48597e51a633d6640ad0188c6ff4ad2934b
Clark Winkelmann <clark.winkelmann@gmail.com>
2023-02-15 23:44:12 +0100

    More flexible host param

--- client/js/app.ts
+++ client/js/app.ts
@@ -3,8 +3,18 @@ import App from './lib/App';
 import Control from './components/Control';
 import Program from './components/Program';
 
+let uri: string = m.parseQueryString(window.location.search).host as string || window.location.hostname;
+if (!/:[0-9+]$/.test(uri)) {
+    // Add default port if unspecified
+    uri += ':8890';
+}
+if (!/^https?:\/\//.test(uri)) {
+    // Add default protocol if unspecified
+    uri = 'http://' + uri;
+}
+
 const app = new App();
-app.connect(m.parseQueryString(window.location.search).host as string || ('http://' + window.location.hostname + ':8890'));
+app.connect(uri);
 
 m.route(document.getElementById('js-app'), '/control', {
     '/control': {
