1395aecad6d135504d57ebe219f5b24707ef897d
Clark Winkelmann <clark.winkelmann@gmail.com>
2023-03-05 00:33:03 +0100

    Try improving 2D projection

--- client/js/components/CameraFlatControl.ts
+++ client/js/components/CameraFlatControl.ts
@@ -32,11 +32,27 @@ export default class CameraFlatControl implements m.ClassComponent<CameraFlatCon
         const x = ((orientation.yaw / 360) + 0.5) * this.width;
         const y = (0.5 - (orientation.pitch / 180)) * this.height;
 
-        const radius = 50;
+        const height = orientation.fov / 180 * this.height;
+        const widthAtEquator = orientation.fov / 360 * this.width;
+        // I just can't figure out the math needed here, but this seems like a good approximation for now
+        const percentLookingUp = (1 - (Math.abs(orientation.pitch - (orientation.fov / 2)) / 90)) * 1.8;
+        const percentLookingDown = (1 - (Math.abs(orientation.pitch + (orientation.fov / 2)) / 90)) * 1.8;
+        const percentLookingMiddle = (1 - (Math.abs(orientation.pitch) / 90));
+        const widthAtTop = widthAtEquator / Math.max(percentLookingDown, 0.01);
+        const widthAtBottom = widthAtEquator / Math.max(percentLookingUp, 0.01);
+        const widthAtMiddle = widthAtEquator / Math.max(percentLookingMiddle, 0.01);
 
         this.context.strokeStyle = color;
         this.context.lineWidth = 2;
-        this.context.strokeRect(x - radius, y - radius, radius * 2, radius * 2);
+        this.context.beginPath();
+        this.context.moveTo(x - (widthAtTop / 2), y - (height / 2));
+        this.context.lineTo(x + (widthAtTop / 2), y - (height / 2));
+        this.context.lineTo(x + (widthAtMiddle / 2), y);
+        this.context.lineTo(x + (widthAtBottom / 2), y + (height / 2));
+        this.context.lineTo(x - (widthAtBottom / 2), y + (height / 2));
+        this.context.lineTo(x - (widthAtMiddle / 2), y);
+        this.context.closePath();
+        this.context.stroke();
     }
 
     view(vnode: m.VnodeDOM<CameraFlatControlAttrs, this>) {
